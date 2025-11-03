e56e32e572dd7c3cfa9b293a374a9a53eca5b2e1
Nicole <ni.ferng@gmail.com>
2025-11-03 10:57:11 -0300

    feat: se agrega otra frase a la funcion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Hola Mundo');
 
 // Función para saludar
 function saludar(nombre) {
-    console.log(`Hola, \${nombre}!`);
+    console.log(`Hola, ${nombre} este es un ejercicio de Git!`);
 }
 
 saludar('Desarrollador');
